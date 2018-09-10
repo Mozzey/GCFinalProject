fe796e5e75ba0e689319ca71ea1ea337973145e8
Rabin Rai <rabinrai44@gmail.com>
2018-09-10 10:29:41 -0400

    Added Database for LetsRaid project with servers and guilds and map

---
+++ LetsRaid/LetsRaid/DAL/Maps/GuildMap.cs
@@ -0,0 +1,15 @@
+﻿using LetsRaid.Models.GuildModels;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace LetsRaid.DAL.Maps
+{
+    public class GuildMap : EntityTypeConfiguration<Guild>
+    {
+        public GuildMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+}
---
+++ LetsRaid/LetsRaid/DAL/Maps/ServerMap.cs
@@ -0,0 +1,16 @@
+﻿using LetsRaid.Models.ServerModels;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace LetsRaid.DAL.Maps
+{
+    public class ServerMap : EntityTypeConfiguration<Server>
+    {
+        public ServerMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            HasMany(x => x.Guilds).WithRequired(x => x.Server).HasForeignKey(x => x.ServerId);
+        }
+    }
+}
