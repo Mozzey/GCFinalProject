961f813503f63e3c93fd56d7a30b324d724a4859
Ross Hinman <hinman.ross@gmail.com>
2018-09-10 15:46:42 -0400

    Started returning server view and working on returning specific guilds for those servers

--- LetsRaid/LetsRaid/Controllers/HomeController.cs
+++ LetsRaid/LetsRaid/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using LetsRaid.DAL;
+using LetsRaid.Models;
+using LetsRaid.Models.GuildModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,9 +28,27 @@ namespace LetsRaid.Controllers
         public ActionResult GuildView()
         {
             var guilds = db.Guilds;
-            return View(guilds.ToList());
+            return View(guilds.ToList().OrderBy(x => x.ServerId));
         }
 
+        public ActionResult Details()
+        {
+            List<Guild> guildList = db.Guilds.ToList();
+            List<ServerVM> guildVmList = guildList.Select(x => new ServerVM
+            {
+                ServerName = x.Server.Name,
+                GuildName = x.Name
+            }).ToList();
+            //var guilds = db.Guilds.Include("Guilds").SingleOrDefault(x => x.ServerId == serverId);
+                return View();
+        }
+
+        public ActionResult Dalaran(int? serverId)
+        {
+            var guilds = db.Guilds.Where(x => x.ServerId == 1);
+                //Include("Guilds").SingleOrDefault(x => x.ServerId == serverId);
+            return View(guilds);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ LetsRaid/LetsRaid/Models/ServerVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LetsRaid.Models
+{
+    public class ServerVM
+    {
+        public string ServerName { get; set; }
+        public string GuildName { get; set; }
+
+    }
+}
