2621d2ef8c8adac550c1f4708620b8e568919ccb
Ross Hinman <hinman.ross@gmail.com>
2018-09-11 11:00:20 -0400

    Catching up to head

--- LetsRaid/LetsRaid/App_Start/RouteConfig.cs
+++ LetsRaid/LetsRaid/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace LetsRaid
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Character", action = "GetCharacter", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
