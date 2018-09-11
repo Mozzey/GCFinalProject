61c7df4bd3c8f3fcd42cac6e0bac755c3bf7b14c
Mozzey <mbcacciano@gmail.com>
2018-09-11 16:18:58 -0400

    cleaning up our garbage

---
+++ LetsRaid/LetsRaid/Controllers/GuildsController.cs
@@ -0,0 +1,38 @@
+﻿using LetsRaid.Clients;
+using LetsRaid.DAL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LetsRaid.Controllers
+{
+    public class GuildsController : Controller
+    {
+        private readonly CharacterClient _client;
+        LetsraidContext _context;
+
+        public GuildsController()
+        {
+            _context = new LetsraidContext();
+            _client = new CharacterClient();
+        }
+        // GET: Guilds
+        public ActionResult Index(int? Id)
+        {
+            //ViewBag.Title = _context.Servers.Find(Id).Name.ToString();
+            var guilds = _context.Guilds.Where(x => x.ServerId == Id);
+            return View(guilds);
+        }
+
+        public async Task<ActionResult> GetMembers()
+        {
+            ViewBag.Thumbnail = ConfigurationManager.AppSettings["ThumbnailEndpoint"];
+            var members = await _client.GetMembers();
+            return View(members);
+        }
+    }
+}
---
+++ LetsRaid/LetsRaid/Controllers/ServersController.cs
@@ -0,0 +1,27 @@
+﻿using LetsRaid.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LetsRaid.Controllers
+{
+    public class ServersController : Controller
+    {
+        LetsraidContext _context;
+
+        public ServersController()
+        {
+            _context = new LetsraidContext();
+        }
+
+        // GET: Servers
+        public ActionResult Index()
+        {
+            var servers = _context.Servers;
+            return View(servers.ToList());
+        }
+
+    }
+}
