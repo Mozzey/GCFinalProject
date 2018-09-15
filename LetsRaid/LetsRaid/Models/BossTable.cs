54dbafa1a020493f5497212c837fab66377c33e8
Mozzey <mbcacciano@gmail.com>
2018-09-15 15:30:41 -0400

    bosses able to be seeded into the DB

--- LetsRaid/LetsRaid/Models/BossTable.cs
+++ LetsRaid/LetsRaid/Models/BossTable.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace LetsRaid.Models.GuildModels
+namespace LetsRaid.Models
 {
     public class BossTable
     {
