8dc74279b9e860ab7873fd996fc578065e94eae6
Rabin Rai <rabinrai44@gmail.com>
2018-09-19 01:17:57 -0400

    Updated filtering data by each column and search with jQuery dataTable API

---
+++ LetsRaid/LetsRaid/Scripts/data-table-filter.js
@@ -0,0 +1,25 @@
+﻿// filter table using jQuery DataTable API
+
+$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#characterTable tfoot th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    });
+
+    // DataTable
+    var table = $('#characterTable').DataTable();
+
+     // apply the search
+    table.columns().every(function () {
+        var that = this;
+
+        $('input', this.footer()).on('keyup change', function () {
+            if (that.search() !== this.value) {
+                that
+                    .search(this.value)
+                    .draw();
+            }
+        });
+    });    
+});
